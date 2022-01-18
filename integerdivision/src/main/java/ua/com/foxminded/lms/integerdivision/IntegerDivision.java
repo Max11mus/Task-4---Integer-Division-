2b3712967ae9e0073a6bdc297dbf6ccacbebad40
DESKTOP-URPJ43V\Max11mus <koseymax@gmail.com>
2022-01-18 09:50:31 +0200

    Refactored. Added helper nested class DivisionStepData.

--- integerdivision/src/main/java/ua/com/foxminded/lms/integerdivision/IntegerDivision.java
+++ integerdivision/src/main/java/ua/com/foxminded/lms/integerdivision/IntegerDivision.java
@@ -4,92 +4,96 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class IntegerDivision {
+	public static final String END_OF_LINE = System.getProperty("line.separator");
 
-	public String calculateColumnDivisionTwoPositiveNumbers(int dividend, int divider) {
-		if (divider == 0) {
-			throw new IllegalArgumentException("ERROR: Divider is Zero.");
-		} else {
-			if ((dividend < 0) || (divider < 0)) {
-				throw new IllegalArgumentException("ERROR: Arguments are Negative.");
-			} else {
-				String resultString = "";
-				ArrayList<String> resultColumnDivision = new ArrayList<String>();
-				String dividendString = intToString(dividend);
-				String dividerString = intToString(divider);
-				String quotientString = intToString(dividend / divider);
-				String remainderString = intToString(dividend % divider);
-				ArrayList<Integer> digitsOfQuotient = new ArrayList<Integer>();
-				ArrayList<Integer> producstOfDividerAndDigitOfQuotient = new ArrayList<Integer>();
+	public String calculateColumnDivision(int dividend, int divider) {
 
-				for (int i = 0; i < quotientString.length(); i++) {
-					digitsOfQuotient.add(Character.digit(quotientString.charAt(i), 10));
-				}
+		checkArguments(dividend, divider);
 
-				for (int i = 0; i < digitsOfQuotient.size(); i++) {
-					producstOfDividerAndDigitOfQuotient.add(divider * digitsOfQuotient.get(i));
-				}
+		ArrayList<String> resultColumnDivision = new ArrayList<String>();
+		String dividendString = Integer.toString(dividend);
+		String dividerString = Integer.toString(divider);
+		String quotientString = Integer.toString(dividend / divider);
+		String remainderString = Integer.toString(dividend % divider);
+		ArrayList<Integer> digitsOfQuotient = new ArrayList<Integer>();
+		ArrayList<Integer> producstOfDividerAndDigitOfQuotient = new ArrayList<Integer>();
 
-				int remainder = dividend % divider;
-				int summ = 0;
-				resultColumnDivision.add(paddingLeftWithSpaceChar(remainderString, dividendString.length() + 1));
-				for (int i = digitsOfQuotient.size() - 1, rightMargin = dividendString.length() + 1; i >= 0; i--) {
-					int product = producstOfDividerAndDigitOfQuotient.get(i);
-					if (digitsOfQuotient.get(i) != 0) {
-						summ = product + remainder;
-						resultColumnDivision.add(0, paddingLeftWithSpaceChar(multipleHyphens(intToString(product).length()), rightMargin));
-						resultColumnDivision.add(0, paddingLeftWithSpaceChar(intToString(product), rightMargin));
-						resultColumnDivision.add(0, paddingLeftWithSpaceChar("_" + intToString(summ), rightMargin));
-						remainder = summ / 10;
-					} else {
-						remainder = remainder / 10;
-					}
-					rightMargin--;
-				}
-				
-				String firstLineColumnDivision = "_" + dividendString + "|" + dividerString;
-				resultColumnDivision.set(0, firstLineColumnDivision);
-				
-				if (divider > dividend) {
-					resultColumnDivision.add(paddingLeftWithSpaceChar("0", dividendString.length() + 1));
-				}
-				String secondLineColumnDivision = resultColumnDivision.get(1);
-				secondLineColumnDivision = paddingRightWithSpaceChar(secondLineColumnDivision, dividendString.length() + 1);
-				secondLineColumnDivision += "|" + multipleHyphens(quotientString.length());
-				resultColumnDivision.set(1, secondLineColumnDivision);
-
-				if (divider > dividend) {
-					resultColumnDivision.add(paddingLeftWithSpaceChar(multipleHyphens(dividendString.length()), dividendString.length() + 1));
-				}
-				String thirdLineColumnDivision = resultColumnDivision.get(2);
-				thirdLineColumnDivision = paddingRightWithSpaceChar(resultColumnDivision.get(2), dividendString.length() + 1);
-				thirdLineColumnDivision += "|" + quotientString;
-				resultColumnDivision.set(2, thirdLineColumnDivision);
-				
-				if (divider > dividend) {
-					resultColumnDivision.add(paddingLeftWithSpaceChar(remainderString, dividendString.length() + 1));
-				}
-				String lastLineColumnDivision = resultColumnDivision.get(resultColumnDivision.size() - 1);
-				String penultLineColumnDivision = resultColumnDivision.get(resultColumnDivision.size() - 2);
-				if (lastLineColumnDivision.length() > penultLineColumnDivision.length()) {
-					lastLineColumnDivision = multipleZeroes(lastLineColumnDivision.length() - penultLineColumnDivision.length());
-					lastLineColumnDivision +=  remainderString;
-					lastLineColumnDivision = paddingLeftWithSpaceChar(lastLineColumnDivision, dividendString.length() + 1);
-					resultColumnDivision.set(resultColumnDivision.size() - 1, lastLineColumnDivision);
-				}
-
-				Iterator<String> iterator = resultColumnDivision.iterator();
-				while (iterator.hasNext()) {
-					resultString += iterator.next() + "\r\n";
-
-				}
-				return resultString;
-			}
+		for (int i = 0; i < quotientString.length(); i++) {
+			digitsOfQuotient.add(Character.digit(quotientString.charAt(i), 10));
 		}
+
+		for (int i = 0; i < digitsOfQuotient.size(); i++) {
+			producstOfDividerAndDigitOfQuotient.add(divider * digitsOfQuotient.get(i));
+		}
+
+		int remainder = dividend % divider;
+		int summ = 0;
+		resultColumnDivision.add(paddingLeftWithSpaceChar(remainderString, dividendString.length() + 1));
+		for (int i = digitsOfQuotient.size() - 1, rightMargin = dividendString.length() + 1; i >= 0; i--) {
+			int product = producstOfDividerAndDigitOfQuotient.get(i);
+			if (digitsOfQuotient.get(i) != 0) {
+				summ = product + remainder;
+				resultColumnDivision.add(0, paddingLeftWithSpaceChar(multipleHyphens(Integer.toString(product).length()), rightMargin));
+				resultColumnDivision.add(0, paddingLeftWithSpaceChar(Integer.toString(product), rightMargin));
+				resultColumnDivision.add(0, paddingLeftWithSpaceChar("_" + Integer.toString(summ), rightMargin));
+				remainder = summ / 10;
+			} else {
+				remainder = remainder / 10;
+			}
+			rightMargin--;
+		}
+
+		String firstLineColumnDivision = "_" + dividendString + "|" + dividerString;
+		resultColumnDivision.set(0, firstLineColumnDivision);
+
+		if (divider > dividend) {
+			resultColumnDivision.add(paddingLeftWithSpaceChar("0", dividendString.length() + 1));
+		}
+		String secondLineColumnDivision = resultColumnDivision.get(1);
+		secondLineColumnDivision = paddingRightWithSpaceChar(secondLineColumnDivision, dividendString.length() + 1);
+		secondLineColumnDivision += "|" + multipleHyphens(quotientString.length());
+		resultColumnDivision.set(1, secondLineColumnDivision);
+
+		if (divider > dividend) {
+			resultColumnDivision.add(paddingLeftWithSpaceChar(multipleHyphens(dividendString.length()), dividendString.length() + 1));
+		}
+		String thirdLineColumnDivision = resultColumnDivision.get(2);
+		thirdLineColumnDivision = paddingRightWithSpaceChar(resultColumnDivision.get(2), dividendString.length() + 1);
+		thirdLineColumnDivision += "|" + quotientString;
+		resultColumnDivision.set(2, thirdLineColumnDivision);
+
+		if (divider > dividend) {
+			resultColumnDivision.add(paddingLeftWithSpaceChar(remainderString, dividendString.length() + 1));
+		}
+		String lastLineColumnDivision = resultColumnDivision.get(resultColumnDivision.size() - 1);
+		String penultLineColumnDivision = resultColumnDivision.get(resultColumnDivision.size() - 2);
+		if (lastLineColumnDivision.length() > penultLineColumnDivision.length()) {
+			lastLineColumnDivision = multipleZeroes(lastLineColumnDivision.length() - penultLineColumnDivision.length());
+			lastLineColumnDivision += remainderString;
+			lastLineColumnDivision = paddingLeftWithSpaceChar(lastLineColumnDivision, dividendString.length() + 1);
+			resultColumnDivision.set(resultColumnDivision.size() - 1, lastLineColumnDivision);
+		}
+
+		return stringArrayToString(resultColumnDivision);
 
 	}
 
-	private String intToString(int input) {
-		return new String(String.valueOf(input));
+	private void checkArguments(int dividend, int divider) {
+		if (divider == 0) {
+			throw new IllegalArgumentException("ERROR: Divider is Zero.");
+		}
+		if ((dividend < 0) || (divider < 0)) {
+			throw new IllegalArgumentException("ERROR: Arguments are Negative.");
+		}
+	}
+
+	private String stringArrayToString(ArrayList<String> stringArray) {
+		String resultString = "";
+		Iterator<String> iterator = stringArray.iterator();
+		while (iterator.hasNext()) {
+			resultString += iterator.next() + END_OF_LINE;
+		}
+		return resultString;
 	}
 
 	private String paddingLeftWithSpaceChar(String input, int length) {
@@ -122,7 +126,6 @@ public class IntegerDivision {
 		} else {
 			return new String("");
 		}
-
 	}
 
 }
--- integerdivision/src/test/java/ua/com/foxminded/lms/integerdivisiontest/integerdivisiontest.java
+++ integerdivision/src/test/java/ua/com/foxminded/lms/integerdivisiontest/integerdivisiontest.java
@@ -1,6 +1,7 @@
 package ua.com.foxminded.lms.integerdivisiontest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ua.com.foxminded.lms.integerdivision.IntegerDivision;
@@ -15,32 +16,32 @@ class IntegerDivisionTest {
 
 	@Test
 	void calculateColumnDivisionTwoPositiveNumbers_mustThrowIllegalArgumentException_whenDividerIsZero() {
-		assertThrows(IllegalArgumentException.class, () -> integerDivision.calculateColumnDivisionTwoPositiveNumbers(100, 0),
+		assertThrows(IllegalArgumentException.class, () -> integerDivision.calculateColumnDivision(100, 0),
 				"Must throw IllegalArgumentException exception when divider is sZero.");
 	}
 
 	@Test
 	void calculateColumnDivisionTwoPositiveNumbers_mustThrowIllegalArgumentException_whenArgumentsAreNegative() {
-		assertThrows(IllegalArgumentException.class, () -> integerDivision.calculateColumnDivisionTwoPositiveNumbers(-100, -50),
+		assertThrows(IllegalArgumentException.class, () -> integerDivision.calculateColumnDivision(-100, -50),
 				"Must throw IllegalArgumentException exception when arguments are negative.");
 	}
 	
 	@Test
 	void calculateColumnDivisionTwoPositiveNumbers_mustReturnSolution_whenDividendGreaterThenDivider() {
-		String expected = "_666846|56\r\n"
-						+ " 56    |-----\r\n"
-						+ " --    |11907\r\n"
-						+ "_106\r\n"
-						+ "  56\r\n"
-						+ "  --\r\n"
-						+ " _508\r\n"
-						+ "  504\r\n"
-						+ "  ---\r\n"
-						+ "   _446\r\n"
-						+ "    392\r\n"
-						+ "    ---\r\n"
-						+ "     54\r\n";
-		String actual = integerDivision.calculateColumnDivisionTwoPositiveNumbers(666846,56);
+		String expected = "_666846|56" + IntegerDivision.END_OF_LINE
+						+ " 56    |-----" + IntegerDivision.END_OF_LINE
+						+ " --    |11907" + IntegerDivision.END_OF_LINE
+						+ "_106" + IntegerDivision.END_OF_LINE
+						+ "  56" + IntegerDivision.END_OF_LINE
+						+ "  --" + IntegerDivision.END_OF_LINE
+						+ " _508" + IntegerDivision.END_OF_LINE
+						+ "  504" + IntegerDivision.END_OF_LINE
+						+ "  ---" + IntegerDivision.END_OF_LINE
+						+ "   _446" + IntegerDivision.END_OF_LINE
+						+ "    392" + IntegerDivision.END_OF_LINE
+						+ "    ---" + IntegerDivision.END_OF_LINE
+						+ "     54" + IntegerDivision.END_OF_LINE;
+		String actual = integerDivision.calculateColumnDivision(666846,56);
 		
 		assertEquals(expected, actual, "Must return solution when dividend greater then divider.");
 	}
@@ -48,74 +49,74 @@ class IntegerDivisionTest {
 	
 	@Test
 	void calculateColumnDivisionTwoPositiveNumbers_mustReturnSolution_whenDividendLessThenDivider() {
-		String expected = "_56|666846\r\n"
-						+ "  0|-\r\n"
-						+ " --|0\r\n"
-						+ " 56\r\n";
-		String actual = integerDivision.calculateColumnDivisionTwoPositiveNumbers(56,666846);
+		String expected = "_56|666846" + IntegerDivision.END_OF_LINE
+						+ "  0|-" + IntegerDivision.END_OF_LINE
+						+ " --|0" + IntegerDivision.END_OF_LINE
+						+ " 56" + IntegerDivision.END_OF_LINE;
+		String actual = integerDivision.calculateColumnDivision(56,666846);
 		
 		assertEquals(expected, actual, "Must return solution when dividend less then divider.");
 	}
 
 	@Test
 	void calculateColumnDivisionTwoPositiveNumbers_mustReturnSolution_whenDividendIsZero() {
-		String expected = "_0|999\r\n"
-						+ " 0|-\r\n"
-						+ " -|0\r\n"
-						+ " 0\r\n";
-		String actual = integerDivision.calculateColumnDivisionTwoPositiveNumbers(0,999);
+		String expected = "_0|999" + IntegerDivision.END_OF_LINE
+						+ " 0|-" + IntegerDivision.END_OF_LINE
+						+ " -|0" + IntegerDivision.END_OF_LINE
+						+ " 0" + IntegerDivision.END_OF_LINE;
+		String actual = integerDivision.calculateColumnDivision(0,999);
 		
 		assertEquals(expected, actual, "Must return solution when when dividend is zero.");
 	}
 	
 	@Test
 	void calculateColumnDivisionTwoPositiveNumbers_mustReturnSolution_whenDividerIsOne() {
-		String expected = "_78654|1\r\n"
-						+ " 7    |-----\r\n"
-						+ " -    |78654\r\n"
-						+ " _8\r\n"
-						+ "  8\r\n"
-						+ "  -\r\n"
-						+ "  _6\r\n"
-						+ "   6\r\n"
-						+ "   -\r\n"
-						+ "   _5\r\n"
-						+ "    5\r\n"
-						+ "    -\r\n"
-						+ "    _4\r\n"
-						+ "     4\r\n"
-						+ "     -\r\n"
-						+ "     0\r\n";
-		String actual = integerDivision.calculateColumnDivisionTwoPositiveNumbers(78654,1);
+		String expected = "_78654|1" + IntegerDivision.END_OF_LINE
+						+ " 7    |-----" + IntegerDivision.END_OF_LINE
+						+ " -    |78654" + IntegerDivision.END_OF_LINE
+						+ " _8" + IntegerDivision.END_OF_LINE
+						+ "  8" + IntegerDivision.END_OF_LINE
+						+ "  -" + IntegerDivision.END_OF_LINE
+						+ "  _6" + IntegerDivision.END_OF_LINE
+						+ "   6" + IntegerDivision.END_OF_LINE
+						+ "   -" + IntegerDivision.END_OF_LINE
+						+ "   _5" + IntegerDivision.END_OF_LINE
+						+ "    5" + IntegerDivision.END_OF_LINE
+						+ "    -" + IntegerDivision.END_OF_LINE
+						+ "    _4" + IntegerDivision.END_OF_LINE
+						+ "     4" + IntegerDivision.END_OF_LINE
+						+ "     -" + IntegerDivision.END_OF_LINE
+						+ "     0" + IntegerDivision.END_OF_LINE;
+		String actual = integerDivision.calculateColumnDivision(78654,1);
 		
 		assertEquals(expected, actual, "Must return solution when divider is one.");
 	}
 	
 	@Test
 	void calculateColumnDivisionTwoPositiveNumbers_mustReturnSolution_whenDividerIsOneDigit() {
-		String expected = "_97654321|7\r\n"
-						+ " 7       |--------\r\n"
-						+ " -       |13950617\r\n"
-						+ "_27\r\n"
-						+ " 21\r\n"
-						+ " --\r\n"
-						+ " _66\r\n"
-						+ "  63\r\n"
-						+ "  --\r\n"
-						+ "  _35\r\n"
-						+ "   35\r\n"
-						+ "   --\r\n"
-						+ "    _43\r\n"
-						+ "     42\r\n"
-						+ "     --\r\n"
-						+ "     _12\r\n"
-						+ "       7\r\n"
-						+ "       -\r\n"
-						+ "      _51\r\n"
-						+ "       49\r\n"
-						+ "       --\r\n"
-						+ "        2\r\n";
-		String actual = integerDivision.calculateColumnDivisionTwoPositiveNumbers(97654321,7);
+		String expected = "_97654321|7" + IntegerDivision.END_OF_LINE
+						+ " 7       |--------" + IntegerDivision.END_OF_LINE
+						+ " -       |13950617" + IntegerDivision.END_OF_LINE
+						+ "_27" + IntegerDivision.END_OF_LINE
+						+ " 21" + IntegerDivision.END_OF_LINE
+						+ " --" + IntegerDivision.END_OF_LINE
+						+ " _66" + IntegerDivision.END_OF_LINE
+						+ "  63" + IntegerDivision.END_OF_LINE
+						+ "  --" + IntegerDivision.END_OF_LINE
+						+ "  _35" + IntegerDivision.END_OF_LINE
+						+ "   35" + IntegerDivision.END_OF_LINE
+						+ "   --" + IntegerDivision.END_OF_LINE
+						+ "    _43" + IntegerDivision.END_OF_LINE
+						+ "     42" + IntegerDivision.END_OF_LINE
+						+ "     --" + IntegerDivision.END_OF_LINE
+						+ "     _12" + IntegerDivision.END_OF_LINE
+						+ "       7" + IntegerDivision.END_OF_LINE
+						+ "       -" + IntegerDivision.END_OF_LINE
+						+ "      _51" + IntegerDivision.END_OF_LINE
+						+ "       49" + IntegerDivision.END_OF_LINE
+						+ "       --" + IntegerDivision.END_OF_LINE
+						+ "        2" + IntegerDivision.END_OF_LINE;
+		String actual = integerDivision.calculateColumnDivision(97654321,7);
 		
 		assertEquals(expected, actual, "Must return solution when divider is one digit.");
 	}
@@ -123,18 +124,17 @@ class IntegerDivisionTest {
 	
 	@Test
 	void calculateColumnDivisionTwoPositiveNumbers_mustReturnSolution_whenQuotientHasTrailingZeroes() {
-		String expected = "_1111000001|11\r\n"
-						+ " 11        |---------\r\n"
-						+ " --        |101000000\r\n"
-						+ "  _11\r\n"
-						+ "   11\r\n"
-						+ "   --\r\n"
-						+ "    0000001\r\n";
+		String expected = "_1111000001|11" + IntegerDivision.END_OF_LINE
+						+ " 11        |---------" + IntegerDivision.END_OF_LINE
+						+ " --        |101000000" + IntegerDivision.END_OF_LINE
+						+ "  _11" + IntegerDivision.END_OF_LINE
+						+ "   11" + IntegerDivision.END_OF_LINE
+						+ "   --" + IntegerDivision.END_OF_LINE
+						+ "    0000001" + IntegerDivision.END_OF_LINE;
 				
-		String actual = integerDivision.calculateColumnDivisionTwoPositiveNumbers(1111000001,11);
+		String actual = integerDivision.calculateColumnDivision(1111000001, 11);
 
 		assertEquals(expected, actual, "Must return solution when quotient has trailing zeroes.");
 	}
-
 
 }
